740be20ff93b10366db0c3558585710cfd6c1f06
Ziniu <guo.ziniu.1003@gmail.com>
2023-07-13 02:05:33 +0800

    site updates

--- data/projectsData.js
+++ data/projectsData.js
@@ -2,8 +2,14 @@ const projectsData = [
   {
     title: 'Rainbow Six Siege Steam/Uplay Version Switcher',
     description: 'A desktop app that helps you switch Rainbow Six Siege installed game version.',
-    imgSrc: '/static/images/switcher.png',
+    imgSrc: '/static/images/r6-switcher/switcher.png',
     href: 'https://github.com/ziniuguo/switcher',
+  },
+  {
+    title: 'Traciege',
+    description: 'Get the daily recap of your siege stats.',
+    imgSrc: '/static/images/traciege/traciege-console.png',
+    href: 'https://github.com/ziniuguo/traciege',
   },
 ]
 export default projectsData
