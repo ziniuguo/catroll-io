d52e22369df65f761f3869b5c0e3b0107ecdc0fc
Ziniu <guo.ziniu.1003@gmail.com>
2023-09-06 22:52:25 +0800

    feat: site changes

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # https://catroll.io
 
+
+## How to build
+Refer to update.sh
 #### Refer to https://github.com/timlrx/tailwind-nextjs-starter-blog for this template used
--- pages/index.js
+++ pages/index.js
@@ -23,14 +23,13 @@ export default function Home({ posts }) {
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="space-y-2 pb-8 pt-6 md:space-y-5">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-            Hi, Welcome to CatRoll's Cattery
+            CatRoll's Cattery
           </h1>
           <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
             {siteMetadata.description}
           </p>
-          <p className="text-sm leading-7 text-gray-500 dark:text-gray-400">More than coding.</p>
           <p className="text-sm leading-7 text-gray-500 dark:text-gray-400">
-            I share my tech discoveries and random shower thoughts here :)
+            Tech discoveries and random shower thoughts.
           </p>
         </div>
         <ul className="divide-y divide-gray-200 dark:divide-gray-700">
